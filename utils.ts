15d3b9f31a52340c4899ce45a41c5cd30e19516e
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-02 23:19:37 +0900

    docs(utils): add docs comment

--- utils.ts
+++ utils.ts
@@ -3,12 +3,15 @@
 
 import { RepresentationHeader } from "./deps.ts";
 
+// TODO(miayuci): add strict parsing.
+
 const ReNoTransform = /(?:^|,)\s*?no-transform\s*?(?:,|$)/;
 
 export function isNoTransform(input: string): boolean {
   return ReNoTransform.test(input);
 }
 
+/** Return new `Response` if the response include `Content-Length` header and readable. */
 export async function reCalcContentLength(
   response: Response,
 ): Promise<Response> {
